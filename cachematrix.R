a1630e19776e0946c5e5b815ce7e5996f4c85480
alazarek <alazarek89@gmail.com>
2016-06-09 13:32:57 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,32 @@
+# clears the console
 clear <- function() {
   cat("\014")
 }
-
+## This function creates a "special" matrix
 makeCacheMatrix <- function (x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(solve) m <<- solve
-  getinverse <- function() m
-  list(set = set, get = get,
-        setinverse = setinverse,
-        getinverse = getinverse)
+  m <- NULL # setting me to null
+  set <- function(y) { # this line sets set to a funtion
+    x <<- y ## setting x to y and creating a "global variable
+    m <<- NULL ## setting m to Null again
+  } # ending the funciton 
+  get <- function() x # setting get to a function returning 1 value x
+  setinverse <- function(solve) m <<- solve ## setting setinverse to a function which applies the solve function to m
+  getinverse <- function() m ## setting getinverse to a function which returns the value of m
+  list(set = set, get = get, ## setting the list of options to be used by an outside call
+        setinverse = setinverse, ## setting setinverse to setinverse
+        getinverse = getinverse)# setting getinverse to getinverse
 }
 
+# this function can only be used after the call of the makeCacheMatrix function is used.
+# this function will return the inverse of a "special" matrix
 cacheSolve <- function (x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+  m <- x$getinverse() #setting m = x
+  if(!is.null(m)) { # was x not null?
+    message("getting cached data") # echo to the console
+    return(m) # exit and return m
+  }# end if
+  data <- x$get() # setting data = x
+  m <- solve(data, ...) # getting the inverse of m
+  x$setinverse(m) # setting the "global" variable so that it stores the inverse
+  m # exit and return m
 }
